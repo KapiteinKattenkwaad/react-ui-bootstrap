df5e313c818807666b851e3e6f90e6bdf5dd1683
Max Stouten <max@liftov.be>
2020-09-08 20:57:50 +0200

    added state

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './../css/App.css';
 
@@ -6,22 +6,54 @@ import AddApointment from './AddAppointment'
 import ListApopintment from './ListApointment'
 import SearchApointment from './SearchAppointment'
 
-function App() {
-  return (
-    <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <AddApointment/>
-              <SearchApointment/>
-              <ListApopintment />
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state =  {
+      myAppointments: []
+    }
+  };
+
+  componentDidMount() {
+    let response = ''
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          return item;
+        })
+        this.setState({
+          myAppointments: apts
+        })
+      })
+
+  }
+
+  render() {
+
+    const listItems = this.state.myAppointments.map(item => (
+      <div>{item.petName}  </div>
+    ));
+
+    return(
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                {listItems}
+                <AddApointment/>
+                <SearchApointment/>
+                <ListApopintment />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    )
+  }
 }
+
 
 export default App;
